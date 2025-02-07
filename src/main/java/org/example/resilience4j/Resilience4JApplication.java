25abb9a312151330af39fa34a6e8e2f34f2a0b13
yokxim <yooksi53@gmail.com>
2025-02-07 19:57:06 +0900

    Initial commit

---
+++ src/main/java/org/example/resilience4j/Resilience4JApplication.java
@@ -0,0 +1,13 @@
+package org.example.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4JApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4JApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/resilience4j/Resilience4JApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4JApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
