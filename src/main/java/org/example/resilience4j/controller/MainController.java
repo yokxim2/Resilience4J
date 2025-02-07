d764e7e22ee14ae578583603b7717229d50d3797
yokxim <yooksi53@gmail.com>
2025-02-07 21:21:47 +0900

    fix[main-controller] Retry 처리 추가

--- src/main/java/org/example/resilience4j/controller/MainController.java
+++ src/main/java/org/example/resilience4j/controller/MainController.java
@@ -1,6 +1,7 @@
 package org.example.resilience4j.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.example.resilience4j.component.Rest1Comp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,7 +20,8 @@ public class MainController {
         this.rest1Comp = rest1Comp;
     }
 
-    @CircuitBreaker(name = "MainControllerMethod1", fallbackMethod = "fallBackMethod")
+    //@CircuitBreaker(name = "MainControllerMethod1", fallbackMethod = "fallBackMethod")
+    @Retry(name = "MainControllerMethod1", fallbackMethod = "fallBackMethod")
     @GetMapping("/")
     public String mainP() {
 
